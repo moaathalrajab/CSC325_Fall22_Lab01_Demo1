b4102f1569d8e9e0006c65101c1764629bb2b654
alrajam@farmingdale.edu <soblab@Whit209-00.it.farmingdale.edu>
2022-09-01 10:27:50 -0400

    Add sayHello method

--- CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
+++ CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
@@ -4,7 +4,10 @@ package edu.farmingdale.csc325_fall22_lab01_demo1;
 
 public class NewClass {
    
+    private static void sayHello(String str){
+        System.out.println(str);
+    }
     public static void main(String[] args) {
-        
+        sayHello("Hello World");
     }
 }
