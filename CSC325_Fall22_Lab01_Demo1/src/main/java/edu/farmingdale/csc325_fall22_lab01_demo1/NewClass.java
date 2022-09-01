51421f2b903288a33846f94699153b7e2b027fb5
alrajam@farmingdale.edu <>
2022-09-01 09:53:51 -0400

    Init class creation

---
+++ CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.farmingdale.csc325_fall22_lab01_demo1;
+
+/**
+ *
+ * @author soblab
+ */
+public class NewClass {
+    
+}
