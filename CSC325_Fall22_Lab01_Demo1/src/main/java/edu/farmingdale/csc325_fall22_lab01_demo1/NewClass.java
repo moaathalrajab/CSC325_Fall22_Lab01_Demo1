1e138ae5415e1e39f68ad0d3104c743f44ee3b60
alrajam@farmingdale.edu <soblab@Whit209-00.it.farmingdale.edu>
2022-09-01 10:06:45 -0400

    Add main method

--- CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
+++ CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.farmingdale.csc325_fall22_lab01_demo1;
 
-/**
- *
- * @author soblab
- */
+
 public class NewClass {
-    
+   
+    public static void main(String[] args) {
+        
+    }
 }
